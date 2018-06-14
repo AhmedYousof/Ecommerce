30771e56505bd3b11950ac2b18505c74e0b86c57
AhmedYousof <31749142+AhmedYousof@users.noreply.github.com>
2018-06-14 04:45:24 +0200

    Edit the port

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
     datapase: 'mongodb://root:abc123@ds261479.mlab.com:61479/mansouramall',
-    port: 3000,
+    port: process.env.PORT || 3000,
     secretKey: 'Ahme@Asad@#$%#!'
 
 }
